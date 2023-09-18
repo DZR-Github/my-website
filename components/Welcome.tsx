1f4fac138175706b6d24510776a6311928e3fd3f
zrDeng <2014358827@qq.com>
2023-09-18 23:19:53 +0800

    init the pages

---
+++ components/Welcome.tsx
@@ -0,0 +1,8 @@
+"use client"
+import {Typewriter} from 'react-simple-typewriter'
+
+export const Welcome=()=>{
+    return (
+        <Typewriter words={['Hello, welcome to my website!']}></Typewriter>
+    )
+}
